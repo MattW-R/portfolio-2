409a025262232673b152c9599a96c98918fd6588
MattWR <Matthew.wr89@gmail.com>
2024-02-01 09:52:34 +0000

    added generation of project panels from project data to root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Projects } from '$lib/data/Projects'
+    import ProjectPanel from '$lib/components/ProjectPanel.svelte'
+</script>
+
+<main>
+    {#each Projects as project}
+        <ProjectPanel data={project} />
+    {/each}
+</main>
