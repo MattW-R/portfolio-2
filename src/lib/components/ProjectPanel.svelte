23f365c7846b184ee5ff32097410c090c1788178
MattWR <Matthew.wr89@gmail.com>
2024-02-01 10:41:32 +0000

    created project panel component

---
+++ src/lib/components/ProjectPanel.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import type { ProjectData } from '$lib/types/ProjectData'
+    import Hyperlink from '$lib/components/Hyperlink.svelte'
+    import TechnologyIcon from '$lib/components/TechnologyIcon.svelte'
+
+    export let data: ProjectData
+</script>
+
+<div>
+    <h2>{data.name}</h2>
+    <p>{data.description}</p>
+    <div>
+        {#each data.links as { displayName, href }}
+            <Hyperlink label={displayName} {href} />
+        {/each}
+    </div>
+    <div>
+        {#each data.techStack as { technology, example }}
+            <TechnologyIcon {technology} href={example} />
+        {/each}
+    </div>
+</div>
