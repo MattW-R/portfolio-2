a7aea799adc89a130193d3ae1fee2473b7cd8fce
MattWR <Matthew.wr89@gmail.com>
2024-02-01 12:13:43 +0000

    refactored headings into component

--- src/lib/components/ProjectPanel.svelte
+++ src/lib/components/ProjectPanel.svelte
@@ -3,12 +3,13 @@
     import Hyperlink from '$lib/components/Hyperlink.svelte'
     import TechnologyPanel from '$lib/components/TechnologyPanel.svelte'
     import Text from '$lib/components/Text.svelte'
+    import Heading from '$lib/components/Heading.svelte'
 
     export let data: ProjectData
 </script>
 
 <div class="w-full max-w-4xl p-inter flex flex-col gap-intra">
-    <h2>{data.name}</h2>
+    <Heading text={data.name} />
     <Text text={data.description} />
     <div class="flex gap-intra">
         {#each data.links as { displayName, href }}
