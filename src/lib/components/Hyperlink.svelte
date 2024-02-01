e46e4eddbd15ea24ce9940792d16e41362e7e667
MattWR <Matthew.wr89@gmail.com>
2024-02-01 12:17:44 +0000

    made hyperlinks open links in new tabs by default

--- src/lib/components/Hyperlink.svelte
+++ src/lib/components/Hyperlink.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     export let href: `https://${string}`
     export let label: string
+    export let openInNewTab: boolean = true
 </script>
 
-<a class="text-accent" {href}>
+<a class="text-accent" {href} target={openInNewTab ? '_blank' : '_self'}>
     {label}
 </a>
