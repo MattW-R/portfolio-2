750bf292bb3a377a0da2eae01b1d18c5709fd53c
MattWR <Matthew.wr89@gmail.com>
2024-02-01 11:34:41 +0000

    configured tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-        extend: {},
+        colors: {
+            transparent: 'transparent',
+            white: '#ffffff',
+            background: '#FFFFEC',
+            primary: '#584721',
+            secondary: '#F1E4C3',
+            accent: '#597E52',
+        },
+        spacing: {
+            inter: '1.2rem',
+            intra: '0.5rem',
+        },
+        fontSize: {
+            h1: '1.9rem',
+            h2: '1.6rem',
+            normal: '1rem',
+        },
+        borderRadius: {},
+        fontFamily: {
+            heading: ['Quicksand', 'sans-serif'],
+            body: ['Source Code Pro', 'monospace'],
+        },
+        borderWidth: {},
     },
     plugins: [],
 }
