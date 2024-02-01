10a23fb6e060ca060d9cc0c8e0c070126840b55c
MattWR <Matthew.wr89@gmail.com>
2024-02-01 10:41:16 +0000

    created technology icon component

---
+++ src/lib/components/TechnologyIcon.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type { DevelopmentTechnology } from '$lib/literals/DevelopmentTechnology'
+
+    export let technology: DevelopmentTechnology
+    export let href: `https://${string}` | undefined
+
+    const getDevIconClassName = (technology: DevelopmentTechnology): string => {
+        switch (technology) {
+            case 'CSS':
+                return 'devicon-css3-plain'
+            case 'HTML':
+                return 'devicon-html5-plain'
+            case 'JavaScript':
+                return 'devicon-javascript-plain'
+            case 'TypeScript':
+                return 'devicon-typescript-plain'
+        }
+    }
+</script>
+
+<a {href}>
+    <i class={getDevIconClassName(technology)}></i>
+</a>
