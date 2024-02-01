ac5de28356e4aef73ed4ce7f0f5d485fb6bcbce2
MattWR <Matthew.wr89@gmail.com>
2024-02-01 12:15:56 +0000

    added title to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import { Projects } from '$lib/data/Projects'
     import ProjectPanel from '$lib/components/ProjectPanel.svelte'
+    import Title from '$lib/components/Title.svelte'
 </script>
 
-<main>
+<main class="flex flex-col justify-start items-center gap-inter p-inter">
+    <Title text="Matt W-R's Portfolio" />
     <div class="flex flex-col justify-start items-center gap-inter">
         {#each Projects as project}
             <ProjectPanel data={project} />
