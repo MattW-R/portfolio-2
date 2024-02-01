1ca220c6f37630cedecc7393a63b0f203f5af6a3
MattWR <Matthew.wr89@gmail.com>
2024-02-01 12:11:47 +0000

    refactored text into component

--- src/lib/components/ProjectPanel.svelte
+++ src/lib/components/ProjectPanel.svelte
@@ -2,13 +2,14 @@
     import type { ProjectData } from '$lib/types/ProjectData'
     import Hyperlink from '$lib/components/Hyperlink.svelte'
     import TechnologyPanel from '$lib/components/TechnologyPanel.svelte'
+    import Text from '$lib/components/Text.svelte'
 
     export let data: ProjectData
 </script>
 
 <div class="w-full max-w-4xl p-inter flex flex-col gap-intra">
     <h2>{data.name}</h2>
-    <p>{data.description}</p>
+    <Text text={data.description} />
     <div class="flex gap-intra">
         {#each data.links as { displayName, href }}
             <Hyperlink label={displayName} {href} />
