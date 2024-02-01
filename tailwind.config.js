d9c489a5effc4bb998d5e6b52fe273fa97b225b0
MattWR <Matthew.wr89@gmail.com>
2024-02-01 12:08:51 +0000

    removed global css declarations

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,21 +10,16 @@ export default {
             secondary: '#F1E4C3',
             accent: '#597E52',
         },
-        spacing: {
-            inter: '1.2rem',
-            intra: '0.5rem',
-        },
-        fontSize: {
-            h1: '1.9rem',
-            h2: '1.6rem',
-            normal: '1rem',
-        },
-        borderRadius: {},
         fontFamily: {
             heading: ['Quicksand', 'sans-serif'],
             body: ['Source Code Pro', 'monospace'],
         },
-        borderWidth: {},
+        extend: {
+            spacing: {
+                inter: '1.2rem',
+                intra: '0.5rem',
+            },
+        },
     },
     plugins: [],
 }
