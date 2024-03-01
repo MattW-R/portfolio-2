2ce6119ec50990101dff632de0269ef186c0bccd
MattWR <Matthew.wr89@gmail.com>
2024-03-01 10:34:55 +0000

    added aptitude test project

--- src/lib/data/Projects.ts
+++ src/lib/data/Projects.ts
@@ -2,6 +2,49 @@ import type { ProjectData } from '$lib/types/ProjectData'
 
 export const Projects: ProjectData[] = [
     {
+        name: 'Aptitude.Test',
+        description:
+            'Worked in one of two parallel agile scrum teams on a legacy TypeScript, SCSS & Bootstrap application. Worked to identify & fix bugs in an existing codebase as well as creating new features in a fashion that complements the existing functionality. Utilised Handlebars to create & dynamically populate templates. Used Postman to assist in the workflow when interacting with a private API.',
+        links: [
+            {
+                displayName: 'GitHub',
+                href: 'https://github.com/iO-Academy/aptitude-test',
+            },
+            {
+                displayName: 'Live',
+                href: 'https://aptitude-test.dev.io-academy.uk/',
+            },
+        ],
+        techStack: [
+            {
+                technology: 'TypeScript',
+                example:
+                    'https://github.com/iO-Academy/aptitude-test/pull/358/files?file-filters%5B%5D=.ts',
+            },
+            {
+                technology: 'SCSS',
+            },
+            {
+                technology: 'HTML',
+                example:
+                    'https://github.com/iO-Academy/aptitude-test/pull/358/files?file-filters%5B%5D=.hbs&file-filters%5B%5D=.html',
+            },
+            {
+                technology: 'handlebars',
+                example:
+                    'https://github.com/iO-Academy/aptitude-test/pull/358/files?file-filters%5B%5D=.hbs',
+            },
+            {
+                technology: 'bootstrap',
+                example:
+                    'https://github.com/iO-Academy/aptitude-test/pull/358/files?file-filters%5B%5D=.hbs&file-filters%5B%5D=.html',
+            },
+            {
+                technology: 'gulp',
+            },
+        ],
+    },
+    {
         name: 'Collection.App',
         description:
             'Created a PHP application to store user inputted item information in a database, with a data structure of my design. Displayed the stored data using HTML & CSS. PHP validation functions were unit tested using PHPUnit & fully documented. Implemented accessible HTML to offer functionality to as many people as possible. Code reviewed peers doing similar projects along the way. Created the database using MySQL & used Docker to run my local development server.',
